6739416378cd39d12a8c63e727d57aaa9bcc2071
Dario Pacule <dariopacule@gmail.com>
2024-08-21 20:58:57 +0200

    cart done

--- app/(home)/cart/page.tsx
+++ app/(home)/cart/page.tsx
@@ -2,9 +2,11 @@
 
 import { BackButton } from "@/components/back-button";
 import { CartProduct } from "@/components/cart-product";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { useAppSelector } from "@/lib/redux";
 import { formatCurrency } from "@/resources/format-currency";
+import Link from "next/link";
 
 export default function Page() {
   const cartItem = useAppSelector((state) => state.cart.items);
@@ -15,7 +17,7 @@ export default function Page() {
     <div className="w-full">
       <BackButton />
 
-      <div className="grid gap-4 w-full grid-cols-3">
+      <div className="grid gap-4 mt-6 w-full grid-cols-3">
         <div className="w-full space-y-2 col-span-2">
           <h1 className="text-2xl">MEU CARRINHO</h1>
           <p>
@@ -26,8 +28,8 @@ export default function Page() {
           </p>
 
           {cartItem.length == 0 && (
-            <div>
-              <p>Voce nao adicionou nenhum item ao carrinho!</p>
+            <div className="flex justify-center w-full underline pt-12">
+              <p>Você ainda não adicionou itens ao carrinho!</p>
             </div>
           )}
 
@@ -38,7 +40,39 @@ export default function Page() {
           </ul>
         </div>
 
-        <Card className="w-full flex-shrink-0 h-[520px]"></Card>
+        <Card className="relative w-full flex-shrink-0 p-4 h-[520px]">
+          <p className="font-bold text-lg">RESUMO DO PEDIDO</p>
+
+          <div className="flex flex-col gap-3 text-sm mt-4">
+            <div className="flex w-full justify-between">
+              <p>Subtotal de produtos</p>
+              <p>MT {formatCurrency(totalPrice)}</p>
+            </div>
+
+            <div className="flex w-full justify-between">
+              <p>Entrega</p>
+              <p>MT {formatCurrency(0)}</p>
+            </div>
+
+            <div className="w-full h-0.5 mt-2 bg-background" />
+          </div>
+
+          <div className="flex font-semibold mt-4 mb-8 w-full justify-between">
+            <p>Total</p>
+            <p>MT {formatCurrency(totalPrice)}</p>
+          </div>
+
+          <Button className="bg-foreground w-full rounded-md">
+            FINALIZAR A COMPRA
+          </Button>
+
+          <Link
+            href="#"
+            className="absolute underline text-muted-foreground bottom-5 left-5"
+          >
+            Ajuda
+          </Link>
+        </Card>
       </div>
     </div>
   );
